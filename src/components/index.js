e9cc5aed3796822c40cb3533c435699876a2edb3
Cadar Maxamed <cadarmaxamed@Cadars-MacBook-Air.local>
2024-03-25 14:36:18 -0700

    working progress, fixing errors

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import CompanyCard from "./CompanyCard";
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Header from "./Header";
+import JobCard from "./JobCard";
+import JobTypes from "./JobTypes";
+import ListBox from "./ListBox";
+import Loading from "./Loading";
+import NavigationBar from "./NavigationBar";
+import SignUp from "./SignUp";
+import TextInput from "./TextInput";
+
+export {
+  CompanyCard,
+  CustomButton,
+  Footer,
+  Header,
+  JobCard,
+  JobTypes,
+  ListBox,
+  Loading,
+  NavigationBar,
+  SignUp,
+  TextInput,
+};
